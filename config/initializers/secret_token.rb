22d51a834925fa691a4921a8ca9815c7491ea036
Informatik <informatik@rats-os.de>
2014-02-12 15:01:19 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Magistros::Application.config.secret_key_base = 'a6f676bcaf322a1f221a3112830001bb52d1a7a9dd2b0b45bdb8a36c3d98f5bf725a5b959286c18c8ee34ffe786bbd952ddca6b4890c04520e5ae2b637cafc93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Magistros::Application.config.session_store :cookie_store, key: '_Magistros_session'
